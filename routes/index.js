91315894d8ace525bd9ca31adcccf292cc6facaf
Rajashree Parhi <rajashreeparhi23@gmail.com>
2019-03-14 20:31:05 +0530

    added login,signup and profile page

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/signup', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('signup');
 });
 
 router.get('/signin', function(req, res, next) {
   res.render('signin');
 });
+router.get('/profile', function(req, res, next) {
+  res.render('profile');
+});
 
+router.get('/loginhere',function(req,res,next){
+  res.redirect("/signin");
+});
+
+router.post('/profile1',function(req,res,next){
+  res.redirect("/profile");
+});
+router.post('/profile2',function(req,res,next){
+  res.redirect("/profile");
+});
 module.exports = router;
